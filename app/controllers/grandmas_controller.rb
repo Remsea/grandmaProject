4eface69b15cd8f7825d47a423f1969446768dd0
pierre etienne bordenave <pierre.etienne.bordenave@gmail.com>
2019-11-12 15:10:47 +0100

    ajout des methodes edit et update dasn le controller grandma + lien updtade dans la vue show

--- app/controllers/grandmas_controller.rb
+++ app/controllers/grandmas_controller.rb
@@ -1,12 +1,13 @@
 class GrandmasController < ApplicationController
+  before_action :set_grandma, only: [:show, :edit, :update, :destroy]
   def show
     @grandma = Grandma.find(params[:id])
   end
-    
+
   def index
     @grandmas = Grandma.all
   end
-  
+
   def new
     @grandma = Grandma.new
   end
@@ -20,9 +21,24 @@ class GrandmasController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @grandla.update(grandma_params)
+      redirect_to @grandma, notice: 'Your Grandma was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def params_grandma
     params.require(:grandma).permit(:name, :description, :price, :address, :age, :competence)
+  end
+
+  def set_grandma
+    @grandma = Grandma.find(params[:id])
   end
 end
