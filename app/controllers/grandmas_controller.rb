ed8710f8598631ed2acd530849315ba0db17d137
Rems <remi2202@zohomail.eu>
2019-11-12 12:56:39 +0100

    adding a search line in the index controller method for grandma

--- app/controllers/grandmas_controller.rb
+++ app/controllers/grandmas_controller.rb
@@ -1,5 +1,12 @@
 class GrandmasController < ApplicationController
   def index
-    @grandmas = Grandma.all
+
+  @grandmas = Grandma.where(competence: params[:grandma][:competence])
+    # @grandmas = Grandma.all
   end
 end
+
+
+# User.where(weekly_subscriber: true).find_each do |user|
+#   NewsMailer.weekly(user).deliver_now
+# end
