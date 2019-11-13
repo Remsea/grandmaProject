6641e567f7c5965fe5d94b33061acea181a76d13
Eva Guillen <eva.guillen6@gmail.com>
2019-11-13 15:26:15 +0100

    change config in photo_uploader and  add photos to index

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,6 +4,19 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :thumnail do
+    resize_to_fit 256, 256
+  end
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:30", radius: 0,
+      width: 45, height: 45, crop: :thumb, gravity: :face
+  end
+
   IDENTIFIERS = ["image/upload/v1573645921/l1ymj0tsdh3bummur0fw.jpg", "image/upload/v1573645922/tzedp2yiquftxsngsjde.jpg", "image/upload/v1573646703/xqrn8xwt5vgmjddhmywv.jpg", "image/upload/v1573647055/slevqxe6jqu5sdmyyff2.jpg", "image/upload/v1573647432/njfxgtirlywgbkshmrw6.jpg", "image/upload/v1573647591/sauhmsophclgyjrdp3ah.jpg"]
 
   def remove!
@@ -11,5 +24,4 @@ class PhotoUploader < CarrierWave::Uploader::Base
     ap IDENTIFIERS
     super unless IDENTIFIERS.map {|id| id.split('/').last }.include?(self.identifier.split('/').last)
   end
-
 end
