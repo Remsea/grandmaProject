af83d3557fb7c27469d7661cc3e3cc659ad82ea3
Eva Guillen <eva.guillen6@gmail.com>
2019-11-15 16:16:43 +0100

    new seed

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -17,7 +17,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
       width: 100, height: 100, crop: :thumb, gravity: :face
   end
 
-  IDENTIFIERS = ["image/upload/v1573645921/l1ymj0tsdh3bummur0fw.jpg", "image/upload/v1573645922/tzedp2yiquftxsngsjde.jpg", "image/upload/v1573646703/xqrn8xwt5vgmjddhmywv.jpg", "image/upload/v1573647055/slevqxe6jqu5sdmyyff2.jpg", "image/upload/v1573647432/njfxgtirlywgbkshmrw6.jpg", "image/upload/v1573647591/sauhmsophclgyjrdp3ah.jpg"]
+  IDENTIFIERS = ["image/upload/v1573645921/l1ymj0tsdh3bummur0fw.jpg", "image/upload/v1573645922/tzedp2yiquftxsngsjde.jpg", "image/upload/v1573646703/xqrn8xwt5vgmjddhmywv.jpg", "image/upload/v1573647055/slevqxe6jqu5sdmyyff2.jpg", "image/upload/v1573647432/njfxgtirlywgbkshmrw6.jpg", "image/upload/v1573647591/sauhmsophclgyjrdp3ah.jpg", "image/upload/v1573828591/vbpzjyqebfame4alqkve.jpg", "image/upload/v1573829086/bedf5njo8d9ojx2csazq.jpg", "image/upload/v1573829519/ouyqovvuvdwsrroyuv5e.jpg"]
 
   def remove!
     ap self.identifier
